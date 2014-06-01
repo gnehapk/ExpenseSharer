735fce0e5522148478d060e90b85a619c54a16f7
nehagupta3 <neha.gupta@pubmatic.com>
2014-06-01 23:51:01 +0530

    research work

--- prototype/angular-seed-master/app/js/controllers.js
+++ prototype/angular-seed-master/app/js/controllers.js
@@ -2,26 +2,78 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('loginForm', function($scope,  $location) {
-  	$scope.users = [{name: "Neha", pwd: "Neha"}, {name: "Sneha", pwd: "Sneha"}];
-  	$scope.login = function (userName, password) {
-  		if (userName === "Neha" && password === "Neha") {
-  			alert("Welcome");
-        $location.path("/home");
-  		} else {
-  			alert("Invalid Login");
-  		}
-  	}
-  })
-  .controller('expenseForm', function($scope, $location) {
-    $scope.expenses = [{
-      exTitle: "",
-      amount: "",
-      typeOfShare: "",
-      date: ""
-    }];
-    $scope.submitExpense = function (expenseInfo) {
-      $scope.expenses.push(expenses.push(JSON.parse(JSON.stringify(expenseInfo)));
+var expmodule = angular.module('myApp.controllers', []);
+
+expmodule.controller('loginForm', function($scope, sharedProperties) {
+    var auth;
+  	$scope.login = function (userid, password) {
+      auth = sharedProperties.session.isAuthenticated(userid, password);
+      if (auth) {
+        alert("Welcome "+userid);
+        window.location = "home.html";
+      } else {
+        alert("Invalid Login");
+      }
+    }
+    $scope.register = function () {
+      window.location = "register.html";
     }
   });
+
+expmodule.controller('expenseForm', function($scope, sharedProperties) {
+  var id, expenses = {};
+  $scope.show = false;
+  $scope.members = [{
+    sNo: "1",
+    id: "gnehapk@gmail.com",
+    name: "Neha",
+    share: 200,
+    paid: 400,
+    owesTo: "200 to Sneha",
+    getsFrom: "400 from Nikita"
+  }, {
+    sNo: "2",
+    id: "gnehapk@gmail.com",
+    name: "Neha",
+    share: 200,
+    paid: 400,
+    owesTo: "200 to Sneha",
+    getsFrom: "400 from Nikita"
+  }];
+  $scope.counter = $scope.members.length + 1;
+  $scope.submitExpense = function (expenseInfo) {
+    id = sharedProperties.session.getInfo();
+    $scope.members = expenseInfo.members = [];
+    if (!expenses[id]) {
+        expenses[id] = [];
+    }
+    expenses[id].push(expenseInfo);
+    alert('Expense Added Successfully');
+  }
+  $scope.addRow = function () {
+    $scope.show = true;
+    return $scope.newRow = true;
+  }
+  $scope.addMember = function (newMember) {
+
+});
+
+expmodule.controller('registrationForm', function($scope, sharedProperties) {
+  var success;
+  $scope.register = function (user) {
+    if (user.pwd !== user.cpwd) {
+      alert("The Confirm Password field does not match the Password field.");
+    } else {
+      success = sharedProperties.addUser(user);
+      if (success) {
+        alert("Successfully Registered.");
+        window.location = "home.html";
+      } else {
+        alert("User already exist with this email.");
+      }
+    }
+  }
+
+  });
+
+
--- prototype/angular-seed-master/app/js/services.js
+++ prototype/angular-seed-master/app/js/services.js
@@ -5,5 +5,60 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngCookies'])
+	.service('sharedProperties', function ($cookieStore) {
+		var users = [{id: "Neha", pwd: "Neha", auth: false}, {id: "Sneha", pwd: "Sneha", auth: false}],
+			authentication = false,
+			expenses = {},
+			currentUser = "";
+
+			function setUserInfo (userid) {
+				$cookieStore.put('userid', userid);
+			}
+			function setAuth () {
+				$cookieStore.put('auth', true);
+			}
+			function getUserInfo () {
+				return $cookieStore.get('userid');
+			}
+
+
+		return {
+			session: {
+				setInfo: function (userid) {
+					setUserInfo(userid);
+				},
+				setAuthentication: function () {
+					setAuth();
+				},
+				getInfo: function () {
+					return getUserInfo();
+				},
+				isAuthenticated: function (userid, password) {
+					var oThis = this, i, length = users.length;
+					for (i = 0; i < length; i++) {
+						if (users[i].id === userid && users[i].pwd === password) {
+        					oThis.setInfo(userid);
+        					oThis.setAuthentication();
+        					return true;
+        				} else {
+        					return false;
+        				}
+        			}
+				}
+			},
+			addUser: function (user) {
+				var length = users.length, success = true, i;
+				for (i = 0; i < length; i++) {
+					if (users[i].id === user.id) {
+    					success = false;
+    					break;
+    				}
+    			}
+    			if (success) {
+    				users.push(user);
+    			}
+    			return success;
+			}
+		}
+});
